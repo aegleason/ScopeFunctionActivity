a4a0df23253fc6a0e1bb816b2b5042c216bf5534
Annie Gleason <tun23140@temple.edu>
2025-02-12 14:20:32 -0500

    Refactored 3 functions in MainActivity (getTestDataArray, averageLessThanMedian, getView) to allow for more concise scope functions/SEF. For lab 2/12

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import kotlin.random.Random
 
@@ -12,6 +14,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testData = getTestDataArray()
+        Log.d("MainActivity", "Generated sorted list: $testData")
+
+        val sampleList = listOf(10, 20, 30, 40, 50)
+        val exampleView = getView(2, null, sampleList, this)
+
+        // findViewById<LinearLayout>(R.id.container).addView(exampleView)
 
         // You can test your helper functions by  calling them from onCreate() and
         // printing their output to the Log, which is visible in the LogCat:
@@ -27,14 +37,19 @@ class MainActivity : AppCompatActivity() {
     // Look at the final/return value and build the function "working backwards"
 
     // Return a list of random, sorted integers
-    private fun getTestDataArray() : List<Int> {
+    /* private fun getTestDataArray() : List<Int> {
         val testArray = MutableList(10){ Random.nextInt()}
         testArray.sort()
         return testArray
     }
+     */
+
+    private fun getTestDataArray(): List<Int> = List(10) { (1..100).random() }.sorted()
+    // SEF for lab 2/12. Sorts list in ascending order. Tried to be as brief as possible
+
 
     // Return true if average value in list is greater than median value, false otherwise
-    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
+    /* private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
         val avg = listOfNumbers.average()
         val sortedList = listOfNumbers.sorted()
         val median = if (sortedList.size % 2 == 0)
@@ -45,8 +60,14 @@ class MainActivity : AppCompatActivity() {
         return avg < median
     }
 
+     */
+
+    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean =
+        listOfNumbers.run { average() < sorted().let { it[it.size / 2] } }
+    // SEF for lab 2/12. average() computes mean, sorted() sorts, etc. let gets median
+
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
+    /* private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
         val textView: TextView
 
         if (recycledView != null) {
@@ -61,5 +82,15 @@ class MainActivity : AppCompatActivity() {
 
         return textView
     }
+
+     */
+
+    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View =
+        (recycledView ?: TextView(context).apply { layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT) }).apply {
+            if (this is TextView) text = collection[position].toString()
+        }
+
+    // if recycledView isn't null then reuse it
+    // apply sets layoutParams and makes sure a recycledView lays out correctly
 
 }
